61e981bcfbe2799ce6ed3ddc2a3458db79d54bed
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-22 00:16:47 +0530

    added the footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import react from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 function App() {
     return (<div>
         <Header />
+        <Footer />
     </div>);
 }
 
