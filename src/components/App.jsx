ba83289a952835abb2fbc6394897e1ced5879b9a
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-22 00:21:50 +0530

    added the note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import react from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Note from "./Note";
+
 function App() {
     return (<div>
         <Header />
+        <Note />
         <Footer />
     </div>);
 }
